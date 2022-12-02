ba91dcdabc2c970d89b601267e78ebb6d4eb0761
finkin <arlekias@gmail.com>
2022-12-03 00:25:57 +0100

    Day 1

---
+++ 1/Program.cs
@@ -0,0 +1,19 @@
+﻿List<int> elves = new List<int>();
+int current = 0;
+
+foreach (string line in File.ReadAllLines("input.txt"))
+{
+    try
+    {
+        current += Int32.Parse(line);
+    }
+    catch
+    {
+        elves.Add(current);
+        current = 0;
+    }
+}
+
+Console.WriteLine($"Solution 1 is {elves.Max()}");
+Console.WriteLine($"Solution 2 is {elves.OrderByDescending(i => i).Take(3).Sum()}");
+
